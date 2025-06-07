bef0ad31fafa21ab00f8c656fd97470e16390dd1
Sarah Greenberg <68124710+bellgreenb@users.noreply.github.com>
2025-06-07 15:07:32 -0400

    Initial commit

---
+++ frmFactorial.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmFactorial
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtFactorial = New System.Windows.Forms.MaskedTextBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtNumber = New System.Windows.Forms.MaskedTextBox()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(43, 48)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(73, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Number: "
+        '
+        'txtFactorial
+        '
+        Me.txtFactorial.Location = New System.Drawing.Point(122, 90)
+        Me.txtFactorial.Name = "txtFactorial"
+        Me.txtFactorial.Size = New System.Drawing.Size(216, 26)
+        Me.txtFactorial.TabIndex = 1
+        Me.txtFactorial.TabStop = False
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(144, 134)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(102, 34)
+        Me.btnCalculate.TabIndex = 1
+        Me.btnCalculate.Text = "&Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(38, 90)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(78, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Factorial: "
+        '
+        'txtNumber
+        '
+        Me.txtNumber.Location = New System.Drawing.Point(122, 48)
+        Me.txtNumber.Name = "txtNumber"
+        Me.txtNumber.Size = New System.Drawing.Size(33, 26)
+        Me.txtNumber.TabIndex = 0
+        '
+        'btnExit
+        '
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(252, 134)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(86, 34)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'frmFactorialCalculator
+        '
+        Me.AcceptButton = Me.btnCalculate
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnExit
+        Me.ClientSize = New System.Drawing.Size(378, 190)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.txtNumber)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.txtFactorial)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmFactorialCalculator"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Factorial Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtFactorial As MaskedTextBox
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtNumber As MaskedTextBox
+    Friend WithEvents btnExit As Button
+End Class
---
+++ frmFactorial.vb
@@ -0,0 +1,20 @@
+﻿Public Class frmFactorial
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim number As Integer = CInt(txtNumber.Text)
+        Dim n As Integer = 1
+        Dim factorial As Long = 1
+
+        While n <= number
+            factorial *= n
+            n += 1
+        End While
+        txtFactorial.Text = factorial.ToString("n0")
+
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
